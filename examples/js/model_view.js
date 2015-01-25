8891b2c34995c6b7401f2f4a88728fc269c5bb81
Tom Yeh <doubleshow@gmail.com>
2015-01-25 16:43:38 -0700

    v2 mockup

--- examples/js/model_view.js
+++ examples/js/model_view.js
@@ -1,5 +1,48 @@
 var craft = require('craft')
 
-var model = craft.model.require('pin')
+craft.use('pin', require('craft-pin'))
+craft.use('board', require('craft-pin'))
 
-craft.viewer.view(model)
+// craft.
+//     // pin = require('craft-pin'),
+//     // board = require('craft-board')
+
+// //var design = "<board><pin></pin><pin></pin></board>"
+
+// // craft.help(pin)
+
+// pin({headSize: 5})
+// pin({headSize: 6})
+// // csg -->
+
+// pin()
+
+// p = function(){
+
+// }
+// p.e = 3
+
+
+// .help()
+
+// craft.use('pin', require('craft-pin'))
+// craft.use('board', require('craft-board'))
+
+// var design = "<board><pin></pin><pin></pin></board>"
+// craft.render(design, function(error, model){
+
+// })
+
+//var model = craft.model.require('pin')
+
+//craft.viewer.view(model)
+
+// board
+// 	pin
+// 	pin
+
+
+//craft.model.lookup('pin')
+
+
+//var require('craft-lib-standard')
